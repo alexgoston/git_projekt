c33006f07259b7fa317288a50a863338373bb7b2
alex <alex.agoston.1227@gmail.com>
2020-12-08 13:36:20 +0100

    Javítás

--- munka4.php
+++ munka4.php
@@ -10,26 +10,11 @@ $a = 10;
 $b = 15;
 $c  = 12;
 
-<<<<<<< HEAD
-print $a + $b + $b;
-=======
-print $a + $b;
 
 // Kivonás
 //--------------------
-
 $x = 35;
 $y = 19;
-
-
 print "<br /><br />" . $x - $y;
->>>>>>> kivonás
-
-
-
-
-
-
-// Itt a vége!
-
+//Ez a kivonás ág
 ?>
